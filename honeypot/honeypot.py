045042270b3f30f40590188460e4dfa4c0363c89
O Polar <38509926+goul4rt@users.noreply.github.com>
2025-03-01 14:13:54 -0300

    feat:pt-br

--- honeypot/honeypot.py
+++ honeypot/honeypot.py
@@ -9,16 +9,16 @@ import os
 
 from redbot.core.utils.chat_formatting import box
 
-# Credits:
-# General repo credits.
-# Thanks to Matt for the cog idea!
+# Créditos:
+# Créditos gerais do repositório.
+# Obrigado ao Matt pela ideia do cog!
 
 _: Translator = Translator("Honeypot", __file__)
 
 
 @cog_i18n(_)
 class Honeypot(Cog):
-    """Create a channel at the top of the server to attract self bots/scammers and notify/mute/kick/ban them immediately!"""
+    """Crie um canal no topo do servidor para atrair bots/scammers e notifique/mute/kick/ban them imediatamente!"""
 
     def __init__(self, bot: Red) -> None:
         super().__init__(bot=bot)
@@ -41,29 +41,29 @@ class Honeypot(Cog):
         _settings: typing.Dict[str, typing.Dict[str, typing.Any]] = {
             "enabled": {
                 "converter": bool,
-                "description": "Toggle the cog.",
+                "description": "Ativar ou desativar o cog.",
             },
             "action": {
                 "converter": typing.Literal["mute", "kick", "ban"],
-                "description": "The action to take when a self bot/scammer is detected.",
+                "description": "A ação a ser tomada quando um bot/scammer é detectado.",
             },
             "logs_channel": {
                 "converter": typing.Union[
                     discord.TextChannel, discord.VoiceChannel, discord.Thread
                 ],
-                "description": "The channel to send the logs to.",
+                "description": "O canal para enviar os logs.",
             },
             "ping_role": {
                 "converter": discord.Role,
-                "description": "The role to ping when a self bot/scammer is detected.",
+                "description": "O cargo a ser mencionado quando um bot/scammer é detectado.",
             },
             "mute_role": {
                 "converter": discord.Role,
-                "description": "The mute role to assign to the self bots/scammers, if the action is `mute`.",
+                "description": "O cargo de mudo a ser atribuído aos bots/scammers, se a ação for `mute`.",
             },
             "ban_delete_message_days": {
                 "converter": commands.Range[int, 0, 7],
-                "description": "The number of days of messages to delete when banning a self bot/scammer.",
+                "description": "O número de dias de mensagens a serem deletadas ao banir um bot/scammer.",
             },
         }
         self.settings: Settings = Settings(
@@ -114,7 +114,7 @@ class Honeypot(Cog):
             pass
         action = config["action"]
         embed: discord.Embed = discord.Embed(
-            title=_("Honeypot — Self Bot/Scammer Detected!"),
+            title=_("Honeypot — Bot/Scammer Detectado!"),
             description=f">>> {message.content}",
             color=discord.Color.red(),
             timestamp=message.created_at,
@@ -132,22 +132,22 @@ class Honeypot(Cog):
                         mute_role := message.guild.get_role(mute_role_id)
                     ) is not None:
                         await message.author.add_roles(
-                            mute_role, reason="Self bot/scammer detected."
+                            mute_role, reason="Bot/scammer detectado."
                         )
                     else:
                         failed = _(
-                            "**Failed:** The mute role is not set or doesn't exist anymore."
+                            "**Falhou:** O cargo de mudo não está definido ou não existe mais."
                         )
                 elif action == "kick":
-                    await message.author.kick(reason="Self bot/scammer detected.")
+                    await message.author.kick(reason="Bot/scammer detectado.")
                 elif action == "ban":
                     await message.author.ban(
-                        reason="Self bot/scammer detected.",
+                        reason="Bot/scammer detectado.",
                         delete_message_days=config["ban_delete_message_days"],
                     )
             except discord.HTTPException as e:
                 failed = _(
-                    "**Failed:** An error occurred while trying to take action against the member:\n"
+                    "**Falhou:** Ocorreu um erro ao tentar tomar uma ação contra o membro:\n"
                 ) + box(str(e), lang="py")
             else:
                 await modlog.create_case(
@@ -157,18 +157,18 @@ class Honeypot(Cog):
                     action_type=action,
                     user=message.author,
                     moderator=message.guild.me,
-                    reason="Self bot/scammer detected.",
+                    reason="Bot/scammer detectado.",
                 )
             embed.add_field(
-                name=_("Action:"),
+                name=_("Ação:"),
                 value=(
                     (
-                        _("The member has been muted.")
+                        _("O membro foi silenciado.")
                         if action == "mute"
                         else (
-                            _("The member has been kicked.")
+                            _("O membro foi expulso.")
                             if action == "kick"
-                            else _("The member has been banned.")
+                            else _("O membro foi banido.")
                         )
                     )
                     if failed is None
@@ -191,13 +191,13 @@ class Honeypot(Cog):
     @commands.guildowner()
     @commands.hybrid_group()
     async def sethoneypot(self, ctx: commands.Context) -> None:
-        """Set the honeypot settings. Only the server owner can use this command for security reasons."""
+        """Defina as configurações do honeypot. Apenas o proprietário do servidor pode usar este comando por razões de segurança."""
         pass
 
     @commands.bot_has_guild_permissions(manage_channels=True)
     @sethoneypot.command(aliases=["makechannel"])
     async def createchannel(self, ctx: commands.Context) -> None:
-        """Create the honeypot channel."""
+        """Crie o canal honeypot."""
         if (
             honeypot_channel_id := await self.config.guild(ctx.guild).honeypot_channel()
         ) is not None and (
@@ -205,7 +205,7 @@ class Honeypot(Cog):
         ) is not None:
             raise commands.UserFeedbackCheckFailure(
                 _(
-                    "The honeypot channel already exists: {honeypot_channel.mention} ({honeypot_channel.id})."
+                    "O canal honeypot já existe: {honeypot_channel.mention} ({honeypot_channel.id})."
                 ).format(honeypot_channel=honeypot_channel)
             )
         honeypot_channel = await ctx.guild.create_text_channel(
@@ -223,36 +223,36 @@ class Honeypot(Cog):
                     view_channel=True, read_messages=True, send_messages=True
                 ),
             },
-            reason=f"Honeypot channel creation requested by {ctx.author.display_name} ({ctx.author.id}).",
+            reason=f"Criação do canal honeypot solicitada por {ctx.author.display_name} ({ctx.author.id}).",
         )
         embed = discord.Embed(
-            title=_("⚠️ DO NOT POST HERE! ⚠️"),
+            title=_("⚠️ NÃO POSTE AQUI! ⚠️"),
             description=_(
-                "An action will be immediately taken against you if you send a message in this channel."
+                "Uma ação será imediatamente tomada contra você se você enviar uma mensagem neste canal."
             ),
             color=discord.Color.red(),
         )
         embed.add_field(
-            name=_("What not to do?"),
-            value=_("Do not send any messages in this channel."),
+            name=_("O que não fazer?"),
+            value=_("Não envie mensagens neste canal."),
             inline=False,
         )
         embed.add_field(
-            name=_("What WILL happen?"),
-            value=_("An action will be taken against you."),
+            name=_("O que ACONTECERÁ?"),
+            value=_("Uma ação será tomada contra você."),
             inline=False,
         )
         embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon)
         embed.set_image(url="attachment://do_not_post_here.png")
         await honeypot_channel.send(
-            content=_("## ⚠️ WARNING ⚠️"),
+            content=_("## ⚠️ AVISO ⚠️"),
             embed=embed,
             files=[discord.File(os.path.join(os.path.dirname(__file__), "do_not_post_here.png"))],
         )
         await self.config.guild(ctx.guild).honeypot_channel.set(honeypot_channel.id)
         await ctx.send(
             _(
-                "The honeypot channel has been set to {honeypot_channel.mention} ({honeypot_channel.id}). You can now start attracting self bots/scammers!\n"
-                "Please make sure to enable the cog and set the logs channel, the action to take, the role to ping (and the mute role) if you haven't already."
+                "O canal honeypot foi definido como {honeypot_channel.mention} ({honeypot_channel.id}). Você pode agora começar a atrair bots/scammers!\n"
+                "Por favor, certifique-se de ativar o cog e definir o canal de logs, a ação a ser tomada, o cargo a ser mencionado (e o cargo de mudo) se você ainda não o fez."
             ).format(honeypot_channel=honeypot_channel)
         )
